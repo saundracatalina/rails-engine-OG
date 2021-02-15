2fa6c2b48b5f0878c9e19f0a2edf58361a759dd8
Saundra <saundracatalina@gmail.com>
2021-02-15 10:10:44 -0700

    Add validation tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Invoice, type: :model do
+  describe "validations" do
+  it { should validate_presence_of :status }
+  it { should validate_presence_of :merchant_id }
+  it { should validate_presence_of :customer_id }
+end
   describe 'relationships' do
     it { should belong_to :customer }
     it { should belong_to :merchant }

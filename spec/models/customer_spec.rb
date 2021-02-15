bd0053fcc1c586f146053eecbc681adaff9cb9da
Saundra <saundracatalina@gmail.com>
2021-02-15 09:46:47 -0700

    Add tests for relationships

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Customer, type: :model do
+  describe 'relationships' do
+    it { should have_many :invoices }
+    it { should have_many(:invoice_items).through(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
+    it { should have_many(:merchants).through(:invoices) }
+  end
+end

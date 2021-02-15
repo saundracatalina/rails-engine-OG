10c4a8ab5a65dbce79ba6e92fa1bcc1506c952a7
Saundra <saundracatalina@gmail.com>
2021-02-15 15:24:54 -0700

    Add test for getting one merchant back by it's id

---
+++ spec/requests/api/v1/merchants/single_merchant_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Merchants', type: :request do
+  it 'can get one merchant back by its id' do
+    merchant = create(:merchant)
+    id = merchant.id
+    get api_v1_merchant_path(merchant)
+
+    merchant_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant_json).to have_key(:id)
+    expect(merchant_json[:id]).to be_an(Integer)
+    expect(merchant_json[:id]).to eq(id)
+    expect(merchant_json).to have_key(:name)
+    expect(merchant_json[:name]).to be_a(String)
+  end
+end

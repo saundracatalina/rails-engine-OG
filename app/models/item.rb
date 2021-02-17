abe4497b726eabf1c254ad21af3823dd04c38468
Saundra <saundracatalina@gmail.com>
2021-02-16 21:21:57 -0700

    Add paginate class method

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,12 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.paginate(per_page, page)
+    default = 20
+    items_per_page = (per_page || default).to_i
+    page_num = ((page || 1).to_i)-1
+
+    limit(items_per_page).offset(items_per_page * page_num)
+  end
 end

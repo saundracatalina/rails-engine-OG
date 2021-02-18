abd982b0ff9e55ea3b62eed4e11f4a62a8cb7f21
Saundra <saundracatalina@gmail.com>
2021-02-18 11:00:32 -0700

    Add tests for happy and sad path for finding a merchant's items

--- spec/requests/api/v1/merchants/single_merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/single_merchant_request_spec.rb
@@ -30,4 +30,43 @@ describe 'Merchants', type: :request do
 
     expect(response.status).to eq(404)
   end
+  it 'returns all records associated with a specific merchant' do
+    merchant = create(:merchant)
+    merchant_2 = create(:merchant)
+    item_1 = Item.create!(name: "spoon",
+                          description: "good for scooping",
+                          unit_price: 1.99,
+                          merchant_id: merchant.id)
+    item_2 = Item.create!(name: "knife",
+                          description: "good for cutting",
+                          unit_price: 2.55,
+                          merchant_id: merchant.id)
+    item_3 = Item.create!(name: "fork",
+                          description: "good for forking",
+                          unit_price: 1.99,
+                          merchant_id: merchant_2.id)
+
+    get api_v1_merchant_items_path(merchant)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    merchant_items = json[:data]
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(merchant_items.count).to eq(2)
+    expect(merchant_items).to eq([merchant_items[0], merchant_items[1]])
+    expect(merchant_items[0][:attributes][:name]).to eq(item_1.name)
+    expect(merchant_items[1][:attributes][:name]).to eq(item_2.name)
+    expect(merchant_items[2]).to eq(nil)
+  end
+  it 'returns a 404 if no items found associated with a merchant' do
+    merchant = create(:merchant)
+
+    get api_v1_merchant_items_path(merchant)
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    merchant_items = json[:data]
+
+    expect(response.status).to eq(404)
+  end
 end

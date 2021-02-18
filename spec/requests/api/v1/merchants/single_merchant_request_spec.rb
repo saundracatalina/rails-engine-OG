774539a9b240658c4458a77cbc9c362c6f895734
Saundra <saundracatalina@gmail.com>
2021-02-18 11:22:36 -0700

    Refactor test to include checking the data structure of the response of a merchant's items

--- spec/requests/api/v1/merchants/single_merchant_request_spec.rb
+++ spec/requests/api/v1/merchants/single_merchant_request_spec.rb
@@ -51,7 +51,23 @@ describe 'Merchants', type: :request do
     json = JSON.parse(response.body, symbolize_names: true)
     merchant_items = json[:data]
 
-    expect(response).to be_successful
+    merchant_items.each do |item|
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+      expect(item).to have_key(:type)
+      expect(item[:type]).to be_a(String)
+      expect(item).to have_key(:attributes)
+      expect(item[:attributes]).to be_a(Hash)
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_an(Integer)
+    end
+    
     expect(response.status).to eq(200)
     expect(merchant_items.count).to eq(2)
     expect(merchant_items).to eq([merchant_items[0], merchant_items[1]])

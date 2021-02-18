3d9331b2a1b5d5f961652d621f9925e907e32a54
Saundra <saundracatalina@gmail.com>
2021-02-18 13:11:09 -0700

    Add status: :not_found for 404 response

--- app/controllers/api/v1/item_merchant_controller.rb
+++ app/controllers/api/v1/item_merchant_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ItemMerchantController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Item.find(params[:item_id]).merchant)
+    begin
+      render json: MerchantSerializer.new(Item.find(params[:item_id]).merchant)
+    rescue ActiveRecord::RecordNotFound => error
+      render json: {error: error.to_s}, status: :not_found
+    end
   end
 end

6ea618584bca229de2471eae8377e8e5b1f3aa4c
Saundra <saundracatalina@gmail.com>
2021-02-17 21:38:16 -0700

    Add dependent: :destroy

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,7 @@ class Invoice < ApplicationRecord
   validates_presence_of :status
   belongs_to :customer
   belongs_to :merchant
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :destroy
   has_many :transactions
   has_many :items, through: :invoice_items
 end

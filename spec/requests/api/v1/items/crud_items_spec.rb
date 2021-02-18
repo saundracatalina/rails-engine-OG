5c5d21d4d30b34a72d37f8d540cd839fb6bbcf66
Saundra <saundracatalina@gmail.com>
2021-02-17 17:58:28 -0700

    Add test can create a new item

---
+++ spec/requests/api/v1/items/crud_items_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Items', type: :request do
+  it 'can create a new item' do
+    merchant = create(:merchant)
+    item_params = ({
+                    "name": "value1",
+                    "description": "value2",
+                    "unit_price": 100.99,
+                    "merchant_id": merchant.id
+                    })
+    headers = {'CONTENT_TYPE' => 'application/json'}
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+end

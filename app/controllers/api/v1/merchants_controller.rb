da9405447400b2db02ddd33609bbd24edd11045f
Saundra <saundracatalina@gmail.com>
2021-02-18 17:50:02 -0700

    Add find action and private helper methods

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::MerchantsController< ApplicationController
     merchants = Merchant.paginate(params[:per_page], params[:page])
     render json: MerchantSerializer.new(merchants)
   end
-  
+
   def show
     begin
       render json: MerchantSerializer.new(Merchant.find(params[:id]))
@@ -11,4 +11,29 @@ class Api::V1::MerchantsController< ApplicationController
       render json: {error: error.to_s}, status: :not_found
     end
   end
+
+  def find
+    render json: MerchantSerializer.new(Merchant.merch_search(name_key, search_fragment))
+  end
+
+  private
+
+  def name_key
+    params.keys[0]
+  end
+
+  def search_fragment
+    params[:name]
+  end
+  # def name_key
+  #   merchant_params.keys[0]
+  # end
+  #
+  # def search_fragment
+  #   merchant_params[:name]
+  # end
+  #
+  # def merchant_params
+  #   params.permit(:name)
+  # end
 end
